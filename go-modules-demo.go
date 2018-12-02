a495ad6077e480c2a2b9876eee2c4e6d750e3e25
duminghui <duminghui@139.com>
2018-12-02 09:20:59 +0800

    v1.0.0

---
+++ go-modules-demo.go
@@ -0,0 +1,9 @@
+// Package gomodulesdemo provides ...
+package gomodulesdemo
+
+import "fmt"
+
+// Print version
+func Print() {
+	fmt.Println("this is version 1.0.0")
+}
---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/duminghui/go-modules-demo
